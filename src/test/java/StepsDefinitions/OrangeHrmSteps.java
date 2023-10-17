62ec8221646198dd4b4df55f0cfb4ac76a886bcc
Aamir Naseer <aamirnaseer498@gmail.com>
2023-10-17 13:11:06 +0500

    Committed Steps Definition Class

---
+++ src/test/java/StepsDefinitions/OrangeHrmSteps.java
@@ -0,0 +1,24 @@
+package StepsDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class OrangeHrmSteps {
+    @Given("Launch chrome browser")
+    public void launchChromeBrowser() {
+    }
+
+    @When("Open orangehrm homepage")
+    public void openOrangehrmHomepage() {
+    }
+
+    @Then("Verify that the logo present on page")
+    public void verifyThatTheLogoPresentOnPage() {
+    }
+
+    @And("Close browser")
+    public void closeBrowser() {
+    }
+}
